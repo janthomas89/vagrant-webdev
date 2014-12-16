3368d5da5a0beef09fa43198640847545710a83b
Jan Thomas <Jan Thomas>
2014-12-16 19:52:27 +0100

    Fixed deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-ERRORSTRING="[Error] Make shure to call deploy script like ./deploy.sh live dry-run|push"
+# Constants needed during deployment
+BASEPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ERRORSTRING="[Error] Make shure to call deploy script like ./deploy.sh live [push]"
 
 # Live environtmenr
 LIVE_SSH_USER="username"
@@ -15,31 +17,38 @@ LIVE_SSH_DEPLOY_DIR="/var/www/sites/www.project.de"
 echo "[start] deploy script"
 
 # Deploy via rsync
-if [ $# -eq 0 ] then
-		echo $ERRORSTRING;
-elif [ $1 == "live" ] then
-	if [[ -z $2 ]] then
+if [ $# -eq 0 ]
+then
+		echo "1"
+		echo $ERRORSTRING
+elif [ $1 == "live" ]
+then
+	if [[ -z $2 ]]
+	then
 		echo "[start] rsync dry-run"
-		rsync --dry-run -az --force --delete --progress --exclude-from=deploy-rsync-exclude -e "ssh -p22" ../www $_LIVE_SSH_USER@LIVE_SSH_HOST:$LIVE_SSH_DEPLOY_DIR
+		rsync --dry-run -avp -r --delete --progress --exclude-from=$BASEPATH/deploy-rsync-exclude -e ssh $BASEPATH/../www/ $LIVE_SSH_USER@$LIVE_SSH_HOST:$LIVE_SSH_DEPLOY_DIR
 		echo "[done] rsync dry-run"
-	elif [ $2 == "push" ] then
+	elif [ $2 == "push" ]
+	then
 		echo "[start] rsync deployment"
-		rsync -az --force --delete --progress --exclude-from=deploy-rsync-exclude -e "ssh -p22" ../www $LIVE_SSH_USER@LIVE_SSH_HOST:$LIVE_SSH_DEPLOY_DIR
+		rsync -avp -r --delete --progress --exclude-from=$BASEPATH/deploy-rsync-exclude -e ssh $BASEPATH/../www/ $LIVE_SSH_USER@$LIVE_SSH_HOST:$LIVE_SSH_DEPLOY_DIR
 		echo "[done] rsync deployment"
 
 		# Call postdeployment.sh via SSH
 		echo "[start] post deployment"
-		ssh $_LIVE_SSH_USER@LIVE_SSH_HOST 'chmod +x $LIVE_SSH_DEPLOY_DIR/bin/post-deployment.sh'
-		ssh $_LIVE_SSH_USER@LIVE_SSH_HOST '$LIVE_SSH_DEPLOY_DIR/bin/post-deployment.sh'
+		ssh $LIVE_SSH_USER@$LIVE_SSH_HOST "chmod +x $LIVE_SSH_DEPLOY_DIR/bin/post-deployment.sh"
+		ssh $LIVE_SSH_USER@$LIVE_SSH_HOST "$LIVE_SSH_DEPLOY_DIR/bin/post-deployment.sh"
 		echo "[end] post deployment"
 	else
-		echo $ERRORSTRING;
+		echo "2"
+		echo $ERRORSTRING
 	fi
 # Define some other environments here ...
 # elif [ $1 == "staging" ] then
 # ...
 else
-	echo $ERRORSTRING;
+	echo "3"
+	echo $ERRORSTRING
 fi
 
 echo "[end] deploy script"
